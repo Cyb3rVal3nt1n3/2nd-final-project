4a3e66a974250e234c1564bdc561c9e0b4d9d071
valentine <june.mwololo@strathmore.edu>
2017-11-03 08:06:07 +0300

    Update signupprocess.php

--- processes/signupprocess.php
+++ processes/signupprocess.php
@@ -1,90 +1,84 @@
- <?
-include 'connect.php';
+ <?php
+  
+      if(isset($_POST['register'])){
+            require_once '../connect.php';
 
- $FirstName="";
- $LastName= "";
- $DateOfBirth="";
- $PhoneNumber="";
- $PatientId="";
- $Age="";
- $NextOfKin="";
- $Password=""
- $error= array();
+            function bcrypt($pass_input){
+    $crypt_options = array(
+      'cost' => 10,
+      'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM)
+    );
+    return password_hash($pass_input, PASSWORD_BCRYPT, $crypt_options);
+  }
 
+          $firstName= htmlspecialchars($_POST["name1"]);
+          $lastName= htmlspecialchars($_POST['name2']);
+          $dateOfBirth= htmlspecialchars($_POST['birth']);
+          $phoneNumber= htmlspecialchars($_POST['phone']);
+          // $patientId= htmlspecialchars($_POST['patientId']);
+          $nextOfKin= htmlspecialchars($_POST['Kin']);
 
-           if(isset($_POST['register'])){
+          $password1= htmlspecialchars($_POST['password1']);
+          $conf_pass = $_POST["conf_pass"];
             
-          $FirstName= mysqli_real_escape_string($_POST["name1"]);
-          $LastName= mysqli_real_escape_string($_POST['name2']);
-          $DateOfBirth= mysqli_real_escape_string($_POST['birth']);
-          $PhoneNumber= mysqli_real_escape_string($_POST['phone']);
-          $PatientId= mysqli_real_escape_string($_POST['PatientId']);
-          $Age= mysqli_real_escape_string($_POST['Age']);
-          $NextOfKin= mysqli_real_escape_string($_POST['kin']);
-          $Password= mysqli_real_escape_string($_POST['Password1']);
-
-
-
-
-// entering the error messages into an array
- if (empty($FirstName)) {
-   array_push($error, "FirstName is a mandatory field");
+            if ($password1 == $conf_pass){
+    
+    $hash_pass = bcrypt($conf_pass);
+    
   
-  }
+         $sql ="INSERT INTO patient(firstName,lastName,dateOfBirth,phoneNumber,patientId,nextOfKin,password)
+        VALUES('$firstName','$lastName','$dateOfBirth','$phoneNumber','$patientId','$nextOfKin','$hash_pass')";
 
-  if (empty($LastName)) {
-   array_push($error, "LastName is a mandatory field");
-  
-  }
-
-  if (empty($DateOfBirth)) {
-   array_push($error, "DateOfBirth is a mandatory field");
-  
-  }
-
-  if (empty($PhoneNumber)) {
-   array_push($error, "PhoneNumber is a mandatory field");
-  
-  }
-
-  if (empty($PatientId)) {
-   array_push($error, "PatientId is a mandatory field");
-  
-  }
-
-  if (empty($Age)) {
-   array_push($error, "Age is a mandatory field");
-  
-  }
-
-  if (empty($NextOfKin)) {
-   array_push($error, "Next Of Kin is a mandatory field");
-  
-  }
-
-  if (empty($Password)) {
-   array_push($error, "Password is a mandatory field"); 
-
-  }
-           
-if (count($error) == 0) {
-  $Password==bcrypt(Password1)
-
-  $sql ="INSERT INTO patient( name1, name2, birth, phone, PatientId, Age, Kin, Password1)
-        VALUES('$FirstName','$LastName','DateOfBirth','PhoneNumber','PatientId','Age','NextOfKin','Password1')"
-
-        mysqli_query($db,$sql);
+        $process_sql = $conn->query($sql);
+        
+        if ($process_sql) {
+          echo "new patient added successfully";
+          header("Location:../read.php"); 
+            } 
+        }else{
+            echo "no record inserted";
+        }
 
 
-header('location: login.php');
+          // entering the error messages into an array
+           if (empty($_POST['name1'])) {
+             array_push($error, "FirstName is a mandatory field");
+            
+            }
 
-}
+            if (empty($_POST['name2'])) {
+             array_push($error, "LastName is a mandatory field");
+            
+            }
 
+            if (empty($_POST['birth'])) {
+             array_push($error, "DateOfBirth is a mandatory field");
+            
+            }
 
+            if (empty($_POST['phone'])) {
+             array_push($error, "PhoneNumber is a mandatory field");
+            
+            }
 
-// }
-/*
+            if (empty($_POST['patientId'])) {
+             array_push($error, "PatientId is a mandatory field");
+            
+            }
 
-*/
-  
-?>
+          //   if (empty($_POST['kin'])) {
+          //  array_push($error, "Next Of Kin is a mandatory field");
+          
+          // }
+
+          if (empty($_POST['password1'])) {
+           array_push($error, "Password is a mandatory field"); 
+
+          }
+      
+ }
+
+        //mysqli_close($conn);
+
+ 
+
