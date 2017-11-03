379cd1a95d11f7d3790dfbefa4b71d97e0240c02
valentine <june.mwololo@strathmore.edu>
2017-11-03 08:04:12 +0300

    Update loginprocess.php

--- processes/loginprocess.php
+++ processes/loginprocess.php
@@ -1,12 +1,24 @@
 <?php
 session_start();
+// if the user is already logged in he/she should be redirected to the homepage
+if (isset($_SESSION['use'])) {
+  header("Location: ../homepage.php");
+}
  require_once '../connect.php';
+
+            function bcrypt($pass_input){
+    $crypt_options = array(
+      'cost' => 10,
+      'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM)
+    );
+    return password_hash($pass_input, PASSWORD_BCRYPT, $crypt_options);
+  }
 
 if (isset($_POST['login'])) {
   
 
-    $userName= $_POST['userName'];
-    $password = sha1($_POST['password']);
+    $userName=htmlspecialchars( $_POST['userName']);
+    $password = htmlspecialchars($_POST['password']);
 //prevents mysqli injection
         $userName = stripcslashes($userName);
         $password = stripcslashes($password);
@@ -14,31 +26,26 @@ if (isset($_POST['login'])) {
         $userName = mysqli_real_escape_string($conn,$userName);
         $password = mysqli_real_escape_string($conn,$password);
 
-// function bcrypt($password){
-//     $crypt_options = array(
-//       'cost' => 10,
-//       'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM)
-//     );
-//     return password_hash($password, PASSWORD_BCRYPT, $crypt_options);
-//   }
+ if ($password == $password){
+    
+    $hash_pass = bcrypt($password);
 
-$sql = "SELECT * FROM users WHERE userName='$userName' AND password='$password'";
+$sql = "SELECT * FROM users WHERE userName='$userName' AND password='$hash_pass'";
 $result = $conn->query($sql);
 
 
 $row = mysqli_fetch_array($result);
-var_dump($row);
-echo "<\br>";
+//checks the value in the row being queried on
 
-if ($row['userName'] == $userName &&  $row['password'] == $password) {
+
+if ($row['userName'] == $userName &&  $row['password'] == $hash_pass) {
   echo "successful login"; 
-   //$_SESSION['user'] = 'UserName';
+   $_SESSION['use'] = $userName;
    header('Location:../homepage.php');
-
+}
 }else{
-  echo  "failed login " . $sql . "<br>" . $conn->error;
+  echo  "failed login " . $sql . "<br/>" . $conn->error;
 }
 
 }
 
-?>
