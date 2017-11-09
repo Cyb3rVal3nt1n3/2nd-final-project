c5222a9f7ed36283356e3b0e2dbd67fc0980ef71
valentine <june.mwololo@strathmore.edu>
2017-11-09 23:07:43 +0300

    Add files via upload

--- processes/recordprocess.php
+++ processes/recordprocess.php
@@ -1,24 +1,24 @@
 <?php
         if(isset($_POST['record'])){
             require_once '../connect.php';
-
+              $patientId = $_POST['patientId'];
           $current_appointment= htmlspecialchars($_POST["currentApp"]);
           $illness= htmlspecialchars($_POST["illness"]);
           $treatment= htmlspecialchars($_POST['treatment']);
           $appointment= htmlspecialchars($_POST['appointment']);     
     
   
-         $sql ="INSERT INTO records ( currentApp,illness,treatment,appointment)
-        VALUES('$current_appointment','$illness','$treatment','$appointment')";
+         $sql ="INSERT INTO records ( patientId, current_appointment,illness,treatment,futureAppointment)
+        VALUES('$patientId', '$current_appointment','$illness','$treatment','$appointment')";
 
-        $process_sql = $conn->query($sql);
         
-        if ($process_sql) {
+        if ($conn->query($sql) == TRUE) {
           echo "new record inserted successfully";
-          header("Location:../patientProfile.php"); 
-            } 
+
+          //header("Location:../patientProfile.php"); 
         }else{
-            echo "no record inserted";
+            echo "no record inserted." .$conn->error;
         }
+      }
   
 ?>
