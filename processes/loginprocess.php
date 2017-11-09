cd3435a3f01a65273e82fcccfd85a5b6d9f45726
valentine <june.mwololo@strathmore.edu>
2017-11-09 06:19:45 +0300

    Add files via upload

--- processes/loginprocess.php
+++ processes/loginprocess.php
@@ -4,48 +4,37 @@ session_start();
 if (isset($_SESSION['use'])) {
   header("Location: ../homepage.php");
 }
- require_once '../connect.php';
+
+ require_once 'connect.php';
 
             function bcrypt($pass_input){
-    $crypt_options = array(
-      'cost' => 10,
-      'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM)
-    );
+               $crypt_options = array(
+                     'cost' => 10,
+                    'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM)
+                       );
     return password_hash($pass_input, PASSWORD_BCRYPT, $crypt_options);
-  }
+         }
 
 if (isset($_POST['login'])) {
-  
-
-    $userName=htmlspecialchars( $_POST['userName']);
-    $password = htmlspecialchars($_POST['password']);
+    $userName=stripcslashes(htmlspecialchars($_POST['userName']));
+    $password = stripcslashes(htmlspecialchars($_POST['password']));
 //prevents mysqli injection
-        $userName = stripcslashes($userName);
-        $password = stripcslashes($password);
-        //
-        $userName = mysqli_real_escape_string($conn,$userName);
-        $password = mysqli_real_escape_string($conn,$password);
 
- if ($password == $password){
-    
-    $hash_pass = bcrypt($password);
+ if (!empty($password)){
 
-$sql = "SELECT * FROM users WHERE userName='$userName' AND password='$hash_pass'";
+$sql = "SELECT * FROM users WHERE userName='$userName'";
 $result = $conn->query($sql);
 
-
 $row = mysqli_fetch_array($result);
-//checks the value in the row being queried on
-
-
-if ($row['userName'] == $userName &&  $row['password'] == $hash_pass) {
-  echo "successful login"; 
+    
+    //checks the value in the row being queried on
+if (password_verify($password, $row['password'])) {
+  echo "successful login";
    $_SESSION['use'] = $userName;
-   header('Location:../homepage.php');
+
+   header('Location: homepage.php');
 }
 }else{
   echo  "failed login " . $sql . "<br/>" . $conn->error;
 }
-
 }
-
